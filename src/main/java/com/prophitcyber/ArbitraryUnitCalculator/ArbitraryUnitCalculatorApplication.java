ed622a70c0cb38f69013b4ba968868fbecc1d4ab
Andy Woodard <a.woodard2013@gmail.com>
2022-01-21 22:43:36 -0500

    Initial Template Upload

---
+++ src/main/java/com/prophitcyber/ArbitraryUnitCalculator/ArbitraryUnitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.prophitcyber.ArbitraryUnitCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitraryUnitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbitraryUnitCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prophitcyber/ArbitraryUnitCalculator/ArbitraryUnitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prophitcyber.ArbitraryUnitCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbitraryUnitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
