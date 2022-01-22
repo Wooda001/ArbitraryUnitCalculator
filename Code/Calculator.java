6d0dab3bdedce07e06bfca903cb7c2f3a6b4d4c1
DanCOps <96542147+DanCOps@users.noreply.github.com>
2022-01-21 23:32:50 -0500

    Update Calculator.java

--- Code/Calculator.java
+++ Code/Calculator.java
@@ -1,13 +1,51 @@
 public class Calculator {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void calculateMile() {
-        System.out.println("Please enter the number of miles so that we may calculate the number of inches in your miles input.");
-        double miletoInch = 63360; //inches
-        if(scanner.hasNextDouble()) {
-            double miles = scanner.nextDouble();
-            double calculation = 63360 * miles;
-            System.out.println("There are " + calculation + " inches in " + miles + " miles.");
+    public static void unitofMeasurement() {
+        System.out.println("Please enter your unit of measurement: ");
+        String choice = scanner.next();
+        switch (choice) {
+            case "cm":
+                System.out.println("Enter the number of centimeters here: ");
+                double chosen = scanner.nextDouble();
+                scanner.nextLine();
+                if (chosen <= 0) {
+                    System.out.println("Please enter a valid value");
+                    break;
+                } else if (chosen > 0) {
+                    System.out.println("Please enter the unit of measurement you would like to calculate to: ");
+                    String nextChoice = scanner.next();
+                    switch (nextChoice) {
+                        case "ft":
+                            double numberofcmInFOOT = 30.48;
+                            double calculation = chosen / numberofcmInFOOT;
+
+                            System.out.println(chosen + "cm is equal to " + calculation + " feet");
+                    }
+                }
+                break;
+            case "in":
+                System.out.println("Enter the number of inches here: ");
+                chosen = scanner.nextDouble();
+                scanner.nextLine();
+                if (chosen <= 0) {
+                    System.out.println("Please enter a valid value");
+                    break;
+                } else if (chosen > 0) {
+                    System.out.println("Please enter the unit of measurement you would like to calculate to: ");
+                    String nextChoice = scanner.next();
+                    switch (nextChoice) {
+                        case "ft":
+                            double numberofinchesInFOOT = 12;
+                            double calculation = chosen / numberofinchesInFOOT;
+
+                            System.out.println(chosen + "in is equal to " + calculation + " feet");
+                    }
+                }
+                break;
+            default:
+                System.out.println("Please enter cm, ft, or in");
+                unitofMeasurement();
         }
     }
 
