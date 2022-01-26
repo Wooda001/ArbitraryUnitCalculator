5b9efa2cf2a86713950bff2355031bdd52b56d7a
Andy Woodard <a.woodard2013@gmail.com>
2022-01-25 21:24:22 -0500

    First Draft Working
    Configured Web GUI & Thymeleaf/Spring Boot to handle arguments from initial page. Built out CalculatedValues object with methods. Reflected results of methods in new html templates.

--- src/main/java/com/prophitcyber/ArbitraryUnitCalculator/controllers/ResultController.java
+++ src/main/java/com/prophitcyber/ArbitraryUnitCalculator/controllers/ResultController.java
@@ -4,17 +4,23 @@ import com.prophitcyber.ArbitraryUnitCalculator.models.CalculatedValues;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ResultController {
 
+    @RequestMapping("/")
+    String getIndex(){
+        return "results/index";
+    }
     private final
     @RequestMapping("/result")
-    String getResult(Model model){
-        CalculatedValues theseValues = new CalculatedValues();
-        model.addAttribute("newValue", theseValues.getNewValue());
-        model.addAttribute("originalUnit", theseValues.getOriginalUnit());
-
+    String getResult(Model model, @RequestParam(value = "originalUnit") String originalUnit, @RequestParam(value = "length") double length, @RequestParam(value = "normalUnit") String normalUnit){
+        CalculatedValues theseValues = new CalculatedValues(originalUnit, normalUnit, length);
+        theseValues.calculateValues();
+        model.addAttribute("factOne", theseValues.generateFact1());
+        model.addAttribute("factTwo", theseValues.generateFact2());
+        model.addAttribute("factThree", theseValues.generateFact3());
         return "results/results";
     }
 }
--- src/main/java/com/prophitcyber/ArbitraryUnitCalculator/models/CalculatedValues.java
+++ src/main/java/com/prophitcyber/ArbitraryUnitCalculator/models/CalculatedValues.java
@@ -1,13 +1,19 @@
 package com.prophitcyber.ArbitraryUnitCalculator.models;
 
+import java.util.Objects;
+
 public class CalculatedValues {
 
     private double newValue;
     private String originalUnit;
+    private String normalUnit;
+    private double length;
 
-    public CalculatedValues() {
-        this.newValue = 200;
-        this.originalUnit = "Banana";
+
+    public CalculatedValues(String originalUnit, String normalUnit, double length) {
+        this.originalUnit = originalUnit;
+        this.normalUnit = normalUnit;
+        this.length = length;
     }
 
     public double getNewValue() {
@@ -17,4 +23,22 @@ public class CalculatedValues {
     public String getOriginalUnit() {
         return originalUnit;
     }
+
+    public void calculateValues() {
+    if (Objects.equals(normalUnit, "cm")){
+        this.newValue = (double) length / 2.54;
+    } else {
+        this.newValue = (double) length;
+    }
+    }
+    public String generateFact1(){
+        return "1 Mile is equal to " + (double) Math.round((63360/getNewValue()) * 100 )/100 + " " + originalUnit + "s";
+    }
+    public String generateFact2(){
+        return "The Statue of Liberty is " + (double) Math.round(((305*12)/getNewValue()) * 100)/100 + " " + originalUnit + "s tall";
+    }
+    public String generateFact3(){
+        return "The Mariana's Trench is " + (double) Math.round(((36201*12)/getNewValue()) * 100 )/100 + " " + originalUnit + "s deep";
+    }
+
 }
